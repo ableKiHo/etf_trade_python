62c5c34a567f6993ed3520f9897e67d9f35dedc5
UmKiHo <able.kiho.um@gmail.com>
2020-07-14 00:29:36 +0900

    매도 조건 보완

--- kiwoom/goal_kiwoom.py
+++ kiwoom/goal_kiwoom.py
@@ -50,6 +50,7 @@ class GoalKiwoom(ParentKiwoom):
         self.etf_info_event_loop.exit()
 
     def get_goal_price_etf(self):
+        self.logging.logger.info("get_goal_price_etf")
         for code in self.cal_target_etf_stock_dict.keys():
             value = self.cal_target_etf_stock_dict[code]
             goal_stock_price = cal_goal_stock_price(value[self.customType.START_PRICE], value[self.customType.LAST_DAY_LAST_PRICE], value[self.customType.LAST_DAY_HIGHEST_PRICE],
@@ -57,6 +58,7 @@ class GoalKiwoom(ParentKiwoom):
             self.cal_target_etf_stock_dict[code].update({self.customType.GOAL_PRICE: goal_stock_price})
 
     def logging_reach_goal_price_etf(self):
+        self.logging.logger.info("logging_reach_goal_price_etf")
         for code in self.cal_target_etf_stock_dict.keys():
             value = self.cal_target_etf_stock_dict[code]
             if 0 < value[self.customType.GOAL_PRICE] <= value[self.customType.LAST_PRICE] :
@@ -67,6 +69,7 @@ class GoalKiwoom(ParentKiwoom):
 
         for sCode in self.cal_target_etf_stock_dict.keys():
             QTest.qWait(4000)
+            self.logging.logger.info("get_etf_stock_info [%s]" % sCode)
             self.dynamicCall("SetInputValue(QString, QString)", self.customType.STOCK_CODE, sCode)
             self.dynamicCall("CommRqData(QString, QString, int, QString)", self.customType.OPT10001, "opt10001", 0, self.screen_etf_stock)
             self.etf_info_event_loop.exec_()
@@ -100,4 +103,4 @@ class GoalKiwoom(ParentKiwoom):
                                                                         self.customType.GOAL_PRICE: ''}})
 
             f.close()
-            self.logging.logger.info("전일자 대상건 파일 처리 완료")
+            self.logging.logger.info("전일자 대상건 파일 처리 완료 > %s" % self.cal_target_etf_stock_dict)
