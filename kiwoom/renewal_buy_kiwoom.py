c88a6f62f4f41ac444164e802fed695f81ee3d87
UmKiHo <able.kiho.um@gmail.com>
2020-07-31 11:11:17 +0900

    영점 조정

--- kiwoom/renewal_buy_kiwoom.py
+++ kiwoom/renewal_buy_kiwoom.py
@@ -32,10 +32,10 @@ class RenewalBuyKiwoom(ParentKiwoom):
         self.buy_search_stock_name = ''
         self.total_cal_target_etf_stock_dict = {}
         self.target_etf_dict = {
-            '252670': {self.customType.STOCK_CODE: '252670', "tic": "120틱", self.customType.STOCK_NAME: 'KODEX 200선물인버스2X', "divide_plus_std_price": 0.5, "max_plus_std_price": 1.0, "min_tic": 3},
+            '252670': {self.customType.STOCK_CODE: '252670', "tic": "120틱", self.customType.STOCK_NAME: 'KODEX 200선물인버스2X', "divide_plus_std_price": 0.5, "max_plus_std_price": 1.0, "min_tic": 4},
             '233740': {self.customType.STOCK_CODE: '233740', "tic": "120틱", self.customType.STOCK_NAME: 'KODEX 코스닥150 레버리지', "divide_plus_std_price": 0.75, "max_plus_std_price": 1.5, "min_tic": 4},
             '122630': {self.customType.STOCK_CODE: '122630', "tic": "120틱", self.customType.STOCK_NAME: 'KODEX 레버리지', "divide_plus_std_price": 0.75, "max_plus_std_price": 1.5, "min_tic": 4},
-            '251340': {self.customType.STOCK_CODE: '251340', "tic": "60틱", self.customType.STOCK_NAME: 'KODEX 코스닥150선물인버스', "divide_plus_std_price": 0.5, "max_plus_std_price": 1.0, "min_tic": 3},
+            '251340': {self.customType.STOCK_CODE: '251340', "tic": "60틱", self.customType.STOCK_NAME: 'KODEX 코스닥150선물인버스', "divide_plus_std_price": 0.5, "max_plus_std_price": 1.0, "min_tic": 4},
         }
 
         self.buy_screen_meme_stock = "3000"
