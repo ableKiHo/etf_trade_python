70811dbb22be709307274ab8086469947fdae8a6
UmKiHo <able.kiho.um@gmail.com>
2020-08-26 10:58:35 +0900

    버그수정

--- kiwoom/day_trading_kiwoom.py
+++ kiwoom/day_trading_kiwoom.py
@@ -246,10 +246,14 @@ class DayTradingKiwoom(ParentKiwoom):
         buy_after_tic_rows = [x for x in rows if x[self.customType.TIGHTENING_TIME] > tightening_time]
         max_highest_price = max([item[self.customType.HIGHEST_PRICE] for item in buy_after_tic_rows])
         goni_price = buy_price + ((max_highest_price - buy_price) / 2)
-        if first_tic_ma20 < goni_price:
-            std_price = first_tic_ma20
-        else:
+        if buy_price + 5 > first_tic_ma20:
             std_price = goni_price
+        else:
+            if first_tic_ma20 < goni_price:
+                std_price = first_tic_ma20
+            else:
+                std_price = goni_price
+
         if std_price >= current_price:
             return copy.deepcopy(first_tic)
 
