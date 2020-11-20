ec953ebbcb44b9574c25ceafc2fd625d10f3efb3
UmKiHo <able.kiho.um@gmail.com>
2020-11-20 11:11:57 +0900

    bug fix

--- kiwoom/day_trading_kiwoom.py
+++ kiwoom/day_trading_kiwoom.py
@@ -1461,16 +1461,21 @@ class DayTradingKiwoom(ParentKiwoom):
                 if meme_gubun == self.customType.SELL and sCode in self.current_hold_etf_stock_dict.keys():
                     del self.current_hold_etf_stock_dict[sCode]
                 else:
-                    if sCode not in self.today_buy_etf_stock_dict.keys() and sCode not in self.current_hold_etf_stock_dict.keys():
-                        self.today_buy_etf_stock_dict.update({sCode: {self.customType.PURCHASE_PRICE: buy_price,
-                                                                      self.customType.TIGHTENING_TIME: get_today_by_format('%Y%m%d%H%M%S'),
-                                                                      self.customType.TOTAL_PURCHASE_PRICE: total_buy_price}})
-                        self.today_buy_stock_real_reg(sCode)
+                    if sCode in self.today_buy_etf_stock_dict.keys():
+                        self.today_buy_etf_stock_dict[sCode].update({self.customType.PURCHASE_PRICE: buy_price,
+                                                                     self.customType.HOLDING_QUANTITY: holding_quantity,
+                                                                     self.customType.PURCHASE_AMOUNT: total_buy_price})
 
                     if sCode in self.current_hold_etf_stock_dict.keys():
                         self.current_hold_etf_stock_dict[sCode].update({self.customType.PURCHASE_PRICE: buy_price,
                                                                         self.customType.HOLDING_QUANTITY: holding_quantity,
                                                                         self.customType.PURCHASE_AMOUNT: total_buy_price})
+
+                    if sCode not in self.today_buy_etf_stock_dict.keys() and sCode not in self.current_hold_etf_stock_dict.keys():
+                        self.today_buy_etf_stock_dict.update({sCode: {self.customType.PURCHASE_PRICE: buy_price,
+                                                                      self.customType.TIGHTENING_TIME: get_today_by_format('%Y%m%d%H%M%S'),
+                                                                      self.customType.TOTAL_PURCHASE_PRICE: total_buy_price}})
+                        self.today_buy_stock_real_reg(sCode)
 
     def call_exit(self):
         sys.exit()
