d93be7f844eb24853539045e126e9fece6a37b45
UmKiHo <able.kiho.um@gmail.com>
2020-12-28 16:44:23 +0900

    half sell

--- kiwoom/day_trading_kiwoom.py
+++ kiwoom/day_trading_kiwoom.py
@@ -638,12 +638,12 @@ class DayTradingKiwoom(ParentKiwoom):
 
                     self.logging.logger.info("realdata_std_higest_info > [%s] >> realdata_std_higest_price:%s / highest_profit_rate:%s" % (sCode, realdata_std_higest_price, highest_profit_rate))
 
-                    if highest_profit_rate >= 5.1 and highest_profit_rate > profit_rate:
+                    if highest_profit_rate >= 6.1 and highest_profit_rate > profit_rate:
                         if (highest_profit_rate - 2.3) <= profit_rate < (highest_profit_rate - 2):
                             self.logging.logger.info("highest_profit_sell_point(5.1) check > [%s] >> %s / %s / %s" % (sCode, current_price, profit_rate, highest_profit_rate))
                             self.realtime_stop_loss_sell(sCode)
 
-                    if highest_profit_rate >= 3.1 and highest_profit_rate > profit_rate:
+                    if highest_profit_rate >= 4.6 and highest_profit_rate > profit_rate:
                         if ((highest_profit_rate / 2) - 0.3) <= profit_rate < (highest_profit_rate / 2):
                             self.logging.logger.info("goni_profit_sell_point(3.1) check > [%s] >> %s / %s / %s" % (sCode, current_price, profit_rate, highest_profit_rate))
                             self.realtime_stop_loss_sell(sCode)
