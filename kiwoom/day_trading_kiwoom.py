9c51a422732c31f15e10b2b9cab3763eeeba4bae
UmKiHo <able.kiho.um@gmail.com>
2021-01-25 11:07:43 +0900

    std half sell

--- kiwoom/day_trading_kiwoom.py
+++ kiwoom/day_trading_kiwoom.py
@@ -862,7 +862,8 @@ class DayTradingKiwoom(ParentKiwoom):
                             self.logging.logger.info("profit_10_half_sell_point check > [%s] >> %s / %s / %s" % (sCode, current_price, profit_rate, highest_profit_rate))
                             current_hold_stock["half_sell_receipt"] = True
                             self.realtime_stop_loss_half_sell(sCode)
-
+                        self.logging.logger.info(
+                            "test[%s] >> half_sell_recepit:%s / profit_rate:%s / half_sell:%s" % (sCode, "half_sell_receipt" not in current_hold_stock, profit_rate, current_hold_stock["half_sell"]))
                         if "half_sell_receipt" not in current_hold_stock and profit_rate >= 5.5 and current_hold_stock["half_sell"] is False:
                             self.logging.logger.info("profit_std_half_sell_point total_chegual_price check > [%s] >> %s " % (sCode, total_chegual_price))
                             if (highest_profit_rate - 0.1) > profit_rate and total_chegual_price > self.half_sell_std_amount:
